e53cc54ce7a743eb2a3192f74de593bf218d39ba
AlejaGuzmanRios <111519886+AlejaGuzmanRios@users.noreply.github.com>
2023-07-17 17:46:27 -0500

    first commit

---
+++ src/main/java/aprendiendoSpringboot/primerProyectoSpringboot/PrimerProyectoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package aprendiendoSpringboot.primerProyectoSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoSpringbootApplication.class, args);
+	}
+
+}
